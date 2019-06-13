464c14beb613bd57ecd906e824ae30f9fbda2352
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-06-13 18:05:08 +0530

    Add files via upload

---
+++ try.js
@@ -0,0 +1,11 @@
+function change(){
+    document.getElementById('jsdemo').innerHTML='Hello';
+    var a = 1;
+    var b = 2;
+    var c = a + b;var d;
+    if(c%2==0)
+    d = "even";
+    else
+    d = "odd";
+    document.getElementById('jsdemo').innerHTML=d;
+}
