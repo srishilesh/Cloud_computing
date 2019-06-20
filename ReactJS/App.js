47fde7bf6cf0f5c73a7a5ed6d747a5a7ea63ec00
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-06-20 13:16:59 +0530

    Add files via upload

---
+++ ReactJS/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Components/greet';
+import Shilesh from './Components/jsclass';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello/>
+      <Shilesh/>
+    </div>
+  );
+}
+
+export default App;
